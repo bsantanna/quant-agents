81459d80ea53a8cd4dde1941a03defdd13ffd507
Bruno Sant'Anna <mail@bsantanna.me>
2025-10-23 14:32:56 +0200

    feat: stocks insider trades data ingestion (1)

--- terraform/elasticsearch/main.tf
+++ terraform/elasticsearch/main.tf
@@ -14,8 +14,8 @@ provider "elasticstack" {
   }
 }
 
-resource "elasticstack_elasticsearch_index_lifecycle" "quant-agents_stocks-eod_policy" {
-  name = "quant-agents_stocks-eod_policy"
+resource "elasticstack_elasticsearch_index_lifecycle" "quant-agents_policy" {
+  name = "quant-agents_policy"
 
   hot {
     set_priority {
@@ -68,7 +68,52 @@ resource "elasticstack_elasticsearch_index_template" "quant-agents_stocks-eod_te
       number_of_replicas = 1
 
       lifecycle = {
-        name = elasticstack_elasticsearch_index_lifecycle.quant-agents_stocks-eod_policy.name
+        name = elasticstack_elasticsearch_index_lifecycle.quant-agents_policy.name
+      }
+    })
+  }
+}
+
+resource "elasticstack_elasticsearch_index_template" "quant-agents_stocks-insider-trades_template" {
+  name = "quant-agents_stocks-insider-trades_template"
+
+  index_patterns = ["quant-agents_stocks-insider-trades_*"]
+
+  template {
+    mappings = jsonencode({
+      dynamic = "strict"
+      properties = {
+        key_ticker = {
+          type = "keyword"
+        }
+        key_acquisition_disposal = {
+          type = "keyword"
+        }
+        text_executive_name = {
+          type = "text"
+        }
+        text_executive_title = {
+          type = "text"
+        }
+        date_reference = {
+          type   = "date"
+          format = "yyyy-MM-dd"
+        }
+        val_share_quantity = {
+          type = "double"
+        }
+        val_share_price = {
+          type = "double"
+        }
+      }
+    })
+
+    settings = jsonencode({
+      number_of_shards   = 1
+      number_of_replicas = 1
+
+      lifecycle = {
+        name = elasticstack_elasticsearch_index_lifecycle.quant-agents_policy.name
       }
     })
   }
