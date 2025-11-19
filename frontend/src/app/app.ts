67bde100ee5942bf44850bb5ce57ee8ef6ceff43
Bruno Sant'Anna <mail@bsantanna.me>
2025-11-19 17:11:09 +0100

    feat: header and footer components

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavigationHeader} from './navigation-header/navigation-header';
+import {NavigationFooter} from './navigation-footer/navigation-footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationHeader, NavigationFooter],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ frontend/src/app/navigation-header/navigation-header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-header',
+  imports: [],
+  templateUrl: './navigation-header.html',
+  styleUrl: './navigation-header.scss',
+})
+export class NavigationHeader {
+
+}
