5a9efed39499fa242277eab7d323dd9b572b68b7
Bruno Sant'Anna <mail@bsantanna.me>
2025-12-05 11:44:57 +0100

    fix: get stats close service query updates

--- app/services/markets_stats.py
+++ app/services/markets_stats.py
@@ -8,21 +8,13 @@ class MarketsStatsService:
 
     async def get_stats_close(self, index_name: str, key_ticker: str, close_date:Optional[str]) -> dict:
 
-        if close_date is not None:
-            search_params = {
-              "id": "get_stats_close",
-              "params": {
+        search_params = {
+            "id": "get_stats_close",
+            "params": {
                 "key_ticker": key_ticker,
                 "close_date": close_date,
-              }
             }
-        else:
-            search_params = {
-              "id": "get_stats_close",
-              "params": {
-                "key_ticker": key_ticker,
-              }
-            }
+        }
 
         response = self.es.search_template(index=index_name, body=search_params)
         return response['aggregations']['recent_stats']['value']
