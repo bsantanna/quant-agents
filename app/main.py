e2511384c7570b40ad7452469afdaac662c888ba
Bruno Sant'Anna <mail@bsantanna.me>
2026-05-21 12:27:48 +0200

    fix: Landing page (10)

--- app/main.py
+++ app/main.py
@@ -89,6 +89,7 @@ def setup_auth(container, application):
                 ".*\\.webmanifest$",
                 ".*\\.txt$",
                 "/insights/",
+                "/waitlist(/|$)",
                 "/signup(/|$)",
                 "/terms(/|$)",
                 "/privacy(/|$)",
